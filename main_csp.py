76c58b467500b217c8f3d6b5f37b59ccd27060a6
sem20f12 Tiianhong Gan (tiagan) <sem20f12@sassauna1.ee.ethz.ch>
2020-03-02 19:13:14 +0100

    Update to CSP-rank model

--- main_csp.py
+++ main_csp.py
@@ -13,7 +13,7 @@ import tensorflow as tf
 import get_data as get
 from filters import load_filterbank
 from csp import generate_projection,generate_eye,extract_feature
-from ranking import dimension_reduction, ranking, channel_selection
+from ranking import dimension_reduction, channel_selection
 
 __author__ = "Michael Hersche and Tino Rellstab"
 __email__ = "herschmi@ethz.ch,tinor@ethz.ch"
@@ -27,15 +27,15 @@ class CSP_Model:
 		self.useCSP = True
 		self.fs = 160. # sampling frequency
 		self.NO_channels = 64 # number of EEG channels
-		self.NO_selected_channels = 24 # number of selected channels
+		self.NO_selected_channels = 38 # number of selected channels
 		self.NO_subjects = 105 # number of subjects
 		self.NO_csp = 12 # Total number of CSP feature per band and timewindow
 
-		self.bw = np.array([4, 8, 16, 26]) # bandwidth of filtered signals
+		self.bw = np.array([26]) # bandwidth of filtered signals
 		# self.filter_bank = self.filter_bank[18:27] # use only 4Hz bands
 		self.ftype = 'butter' # 'fir', 'butter'
 		self.forder= 2 # 4
-		self.filter_bank = load_filterbank(self.bw,self.fs,order=self.forder,max_freq=32,ftype = self.ftype) # get filterbank coeffs
+		self.filter_bank = load_filterbank(self.bw,self.fs,order=self.forder,max_freq=30,ftype = self.ftype) # get filterbank coeffs
 		self.NO_bands = self.filter_bank.shape[0]
 
 		time_windows_flt = np.array([
@@ -49,7 +49,7 @@ class CSP_Model:
 									[1,3],
 									[0,3]])*self.fs # time windows in [s] x fs for using as a feature
 		self.time_windows = time_windows_flt.astype(int)
-		# self.time_windows = self.time_windows[8] # use only largest timewindow
+		self.time_windows = self.time_windows[8] # use only largest timewindow
 		self.NO_time_windows = int(self.time_windows.size/2)
 
 		self.NO_features = self.NO_csp*self.NO_bands*self.NO_time_windows
@@ -73,32 +73,37 @@ def main():
 	model = CSP_Model()
 
 	# obtaining the set of 12 spatial filters across an average of all subjects.
-
-	w_sum = 0 # sum of filters for each subject
-
-	for model.subject in range(1,model.NO_subjects+1):
-	# for model.subject in range(1,3): #test
-		start = time.time()
-		model.load_data()
-		w_sum += model.run_csp() # adding filter of individual subject to sum
-		end = time.time()
-		print("Subject " + str(model.subject)+": Time elapsed = " + str(end - start) + " s")
-
-	w_avg_4d = w_sum / 105 # calculating average of filters
-
-	w_avg = dimension_reduction(w_avg_4d, model.NO_channels, model.NO_csp)
-	print(w_avg)
-	np.savetxt(f'w_avg.csv', w_avg) # saving file
+	#
+	# w_sum = 0 # sum of filters for each subject
+	#
+	# for model.subject in range(1,model.NO_subjects+1):
+	# # for model.subject in range(1,3): #test
+	# 	start = time.time()
+	# 	model.load_data()
+	# 	w_sum += model.run_csp() # adding filter of individual subject to sum
+	# 	end = time.time()
+	# 	print("Subject " + str(model.subject)+": Time elapsed = " + str(end - start) + " s")
+	#
+	# w_avg_4d = w_sum / 105 # calculating average of filters
+	#
+	# # w_avg = dimension_reduction(w_avg_4d, model.NO_channels, model.NO_csp) # for multigrade CSP
+	# w_avg = w_avg_4d[0][0]
+	# print(w_avg)
+	# np.savetxt(f'w_avg.csv', w_avg) # saving file
 
 	# channel selection from saved spatial filters
-	# w_avg = np.loadtxt(open("w_avg.csv", "rb"), delimiter=" ")
-	#
+	w_avg = np.loadtxt(open("w_avg.csv", "rb"), delimiter=" ")
+
+	# # V1 using ranking with average energy
 	# w_squared_sum_sorted = ranking(w_avg, model.NO_channels)
 	# # print(w_squared_sum_sorted)
-	#
 	# selected_channels = channel_selection(w_squared_sum_sorted, model.NO_channels, model.NO_selected_channels)
-	# print("The selected channels are: ")
-	# print(selected_channels)
+
+	# V2 using CSP-ranking
+	selected_channels = channel_selection(w_avg, model.NO_channels, model.NO_selected_channels, model.NO_csp)
+
+	print("The selected channels are: ")
+	print(selected_channels)
 
 if __name__ == '__main__':
 	main()
--- ranking.py
+++ ranking.py
@@ -11,7 +11,7 @@ def dimension_reduction(w_avg_4d, NO_channels, NO_csp):
     ''' reducing the dimensions of w_avg from 4D to 2D by taking the modulus
 
     given the 4D input matrix:
-       1. for every time window and frequency, there is a 2D array of NO_channel x NO_csp
+       1. for every time window and frequency, there is a 2D array of NO_channels x NO_csp
        2. take every element of every 2D array, square it, and add to sum matrix at the position of that element
        3. take the square root of each element in the final 2D array to obtain modulus of that element
 
@@ -41,43 +41,88 @@ def dimension_reduction(w_avg_4d, NO_channels, NO_csp):
 
     return np.sqrt(w_avg_2d)
 
+def channel_selection(w_avg, NO_channels, NO_selected_channels, NO_csp):
+    ''' ranking and channel selection using the CSP-rank method
 
-def ranking(w_avg, NO_channels):
-    ''' ranking the energy of each channel obtained from the set of 12 spatial filters
+    The CSP-rank method first sorts the absolute value of the filter coefficients in each filter respectively,
+    then takes the electrode with the next largest coefficient in turn from the 12 spatial filters.
 
      Keyword arguments:
      w_avg -- set of 12 spatial filters obtained from a average of filters obtained from each subject of size [NO_channels, NO_csp]
-     NO_channels - total number of channels
+     NO_channels -- total number of channels
+     NO_selected_channels -- number of channels to select
+     NO_csp -- number of spatial filters
 
-     Return: channels sorted by energy usage of size [NO_channels, 2]
+     Return: 'NO_selected_channels' channels with the highest importance.
     '''
-    w_squared_sum = np.zeros((NO_channels,2)) # creating an empty of dimension NO_channels x 2 for channel number and energy
 
-    index = 0 # iterator
+    sort_temp = np.zeros((NO_channels, 2)) # creating an empty array of dimension NO_channels x 2 for channel number and importance
+    w_avg_sorted = np.zeros((NO_channels, NO_csp)) # creating an empty array of dimension NO_channels x NO_csp for channel selection
 
-    for channel in w_avg:
-        w_squared_sum[index][0] = index + 1 # set channel number
-        for filter in channel:
-            w_squared_sum[index][1] += filter**2 # set channel energy
-        index+=1
+    filter_index = 0
+    channel_index = 0
+    w_avg = w_avg.transpose()
+    for filter in w_avg:
+        channel_index = 0
+        for channel in filter:
+            sort_temp[channel_index][0] = channel_index
+            sort_temp[channel_index][1] = np.sqrt(channel**2)
+            channel_index+=1
+        w_avg_sorted[:, filter_index] = sort_temp[sort_temp[:,1].argsort()][::-1][:, 0] # sort importance in descending order
+        filter_index+=1
 
-    w_squared_sum_sorted = w_squared_sum[w_squared_sum[:,1].argsort()][::-1] # sort energy in descending order
+    selected_channels = np.ones(NO_selected_channels) * 404
+    selected_index = 0
+    filter_index = 0
+    w_avg_channel_index = np.zeros(12)
+    while selected_index < NO_selected_channels:
+        if filter_index == 12:
+            filter_index = 0
+        while w_avg_sorted[int(w_avg_channel_index[filter_index])][filter_index] in selected_channels:
+            w_avg_channel_index[filter_index] += 1
+        selected_channels[selected_index] = w_avg_sorted[int(w_avg_channel_index[filter_index])][filter_index]
+        selected_index += 1
+        filter_index += 1
 
-    return w_squared_sum_sorted
+    return np.sort(selected_channels)
 
 
-def channel_selection(w_squared_sum_sorted, NO_channels, NO_selected):
-    ''' select channels with highest energy usage
-
-    Keyword arguments:
-    w_squared_sum_sorted -- channels sorted by energy usage of size [NO_channels, 2]
-    NO_selected -- number of channels to select
-
-    Return: 'NO_selected' channels with the highest energy usage
-    '''
-    if NO_selected <= NO_channels:
-        selected_channels = np.sort(w_squared_sum_sorted[0:NO_selected, 0])
-    else:
-        selected_channels = np.sort(w_squared_sum_sorted[0:NO_channels, 0])
-
-    return selected_channels
+# def ranking(w_avg, NO_channels):
+#     ''' ranking the energy of each channel obtained from the set of 12 spatial filters
+#
+#      Keyword arguments:
+#      w_avg -- set of 12 spatial filters obtained from a average of filters obtained from each subject of size [NO_channels, NO_csp]
+#      NO_channels - total number of channels
+#
+#      Return: channels sorted by energy usage of size [NO_channels, 2]
+#     '''
+#     w_squared_sum = np.zeros((NO_channels,2)) # creating an empty of dimension NO_channels x 2 for channel number and energy
+#
+#     index = 0 # iterator
+#
+#     for channel in w_avg:
+#         w_squared_sum[index][0] = index + 1 # set channel number
+#         for filter in channel:
+#             w_squared_sum[index][1] += filter**2 # set channel energy
+#         index+=1
+#
+#     w_squared_sum_sorted = w_squared_sum[w_squared_sum[:,1].argsort()][::-1] # sort energy in descending order
+#
+#     return w_squared_sum_sorted
+#
+#
+# def channel_selection(w_squared_sum_sorted, NO_channels, NO_selected_channels):
+#     ''' select channels with highest energy usage
+#
+#     Keyword arguments:
+#     w_squared_sum_sorted -- channels sorted by energy usage of size [NO_channels, 2]
+#     NO_selected_channels -- number of channels to select
+#
+#     Return: 'NO_selected_channels' channels with the highest energy usage
+#     '''
+#     if NO_selected_channels <= NO_channels:
+#         selected_channels = np.sort(w_squared_sum_sorted[0:NO_selected_channels, 0])
+#     else:
+#         selected_channels = np.sort(w_squared_sum_sorted[0:NO_channels, 0])
+#
+#     return selected_channels
