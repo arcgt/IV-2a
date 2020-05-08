7e1f2b814b3de5370f60dc205edadd5050dfff90
Tianhong Gan <tianhonggan@outlook.com>
2020-05-08 17:41:43 +0200

    add heatmap for each filter

--- plot_channels.py
+++ plot_channels.py
@@ -1,9 +1,6 @@
 import os
 import numpy as np
 from PIL import Image
-from keras.models import load_model
-
-from ranking import get_cspweights
 
 __author__ = "Tianhong Gan"
 __email__ = "tianhonggan@outlook.com"
@@ -24,31 +21,82 @@ def plot_channels(channels, num_classes, n_ch):
         background.paste(img, (0, 0), img)
     background.save(f'plot_channels/plots/class{num_classes}_nch{n_ch}.png',"PNG")
 
-def plot_heatmap(num_classes):
+def plot_heatmap_avg(num_classes,type):
+    ''' plot heatmap of 64-channels on 10-10 international system - average across all 12 filters
+
+    Keyword arguments:
+    num_classes -- number of classes
+    type -- 'channels' for coloured ring around channels or 'channels_fill' for opaque colour heatmap
+    '''
     os.makedirs(f'plot_channels/plots/heatmap', exist_ok=True)
 
-    w = np.loadtxt(open(f'results/w_{num_classes}class_csp.csv', "rb"), delimiter=" ")
-    w = get_cspweights(w)
+    w_temp = np.loadtxt(open(f'results/w_{num_classes}class_csp.csv', "rb"), delimiter=" ")
+    w = np.zeros(64) # creating an empty of dimension 64 x 2 for channel number and energy
+
+    index = 0 # iterator
+
+    for channel in w_temp:
+        for filter in channel:
+            w[index] += np.sqrt(filter**2) # set channel energy
+        index+=1
+
+    mean = np.mean(w)
+    sd = np.std(w)
 
     background = Image.open(f'plot_channels/64_channel_sharbrough_bg.png')
 
     channel = 1
     for i in w:
-        if i > 1:
-            img = Image.open(f'plot_channels/channels/{channel}.png')
+        if i > mean+sd:
+            img = Image.open(f'plot_channels/{type}/{channel}.png')
             background.paste(img, (0, 0), img)
-        elif i > 0.6:
-            img = Image.open(f'plot_channels/channels/{channel}_o.png')
+        elif i > mean:
+            img = Image.open(f'plot_channels/{type}/{channel}_o.png')
             background.paste(img, (0, 0), img)
-        elif i > 0.4:
-            img = Image.open(f'plot_channels/channels/{channel}_y.png')
+        elif i > mean-sd:
+            img = Image.open(f'plot_channels/{type}/{channel}_y.png')
             background.paste(img, (0, 0), img)
         else:
             pass
         channel += 1
     background.save(f'plot_channels/plots/heatmap/class{num_classes}.png',"PNG")
 
-# plot_heatmap(4)
+def plot_heatmap(num_classes,type):
+    ''' plot heatmap of 64-channels on 10-10 international system - for all 12 csp filters
+
+    Keyword arguments:
+    num_classes -- number of classes
+    type -- 'channels' for coloured ring around channels or 'channels_fill' for opaque colour heatmap
+    '''
+    os.makedirs(f'plot_channels/plots/heatmap', exist_ok=True)
+
+    w = np.loadtxt(open(f'results/w_{num_classes}class_csp.csv', "rb"), delimiter=" ")
+
+    mean = np.mean(w)
+    sd = np.std(w)
+
+    for filter in range(12):
+        background = Image.open(f'plot_channels/64_channel_sharbrough_bg.png')
+
+        channel = 1
+        for i in w[:,filter]:
+            if i > mean+sd:
+                img = Image.open(f'plot_channels/{type}/{channel}.png')
+                background.paste(img, (0, 0), img)
+            elif i > mean:
+                img = Image.open(f'plot_channels/{type}/{channel}_o.png')
+                background.paste(img, (0, 0), img)
+            elif i > mean-sd:
+                img = Image.open(f'plot_channels/{type}/{channel}_y.png')
+                background.paste(img, (0, 0), img)
+            else:
+                pass
+            channel += 1
+        background.save(f'plot_channels/plots/heatmap/class{num_classes}_filter{filter}.png',"PNG")
+
+# plot_heatmap_avg(4,'channels_fill')
+# plot_heatmap(4,'channels_fill')
+
 
 ''' make white background transparent '''
 
